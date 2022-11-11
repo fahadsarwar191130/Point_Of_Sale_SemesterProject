53d445d776178f3ff86d9fd929d3c0d8fe9b73f6
Fahad bin Sarwar <85784549+fahadsarwar191130@users.noreply.github.com>
2022-11-11 15:43:14 +0500

    Add files via upload

---
+++ POS_Querries.sql
@@ -0,0 +1,21 @@
+create database POS
+
+create table Products
+(
+Id int,
+ProductName varchar(50),
+ProductCategory varchar(50),
+Price int,
+Quantity int
+)
+
+Create table Login
+(
+UserName varchar(50),
+Password varchar(50)
+)
+
+select * from Products
+
+select * from Login
+
